d3b21278d31161e686481875a43d2fa322ee451a
clearbluejar <3752074+clearbluejar@users.noreply.github.com>
2023-04-06 16:14:30 -0400

    another1

--- get_verinfo.ps1
+++ get_verinfo.ps1
@@ -1,27 +1,130 @@
-        $ver = [System.Environment]::OSVersion.Version -join '.'
-        $bin_types = "*.exe","*.dll","*.sys","*.winmd","*.cpl","*.ax","*.node","*.ocx","*.efi","*.acm","*.scr","*.tsp","*.drv"
-        $allpaths_to_scan = "C:\Program*\Mi*","C:\Program*\Win*", "C:\Windows\System32\", "C:\Program*\Common*"
-        #$allpaths_to_scan = "C:\\Program Files\\Microsoft Office\\root\\vfs\\ProgramFilesCommonX64\\Microsoft Shared\\OFFICE16\\AppvIsvSubsystems64.dll"
-        $all_files = Get-ChildItem $allpaths_to_scan -Include $bin_types -Recurse -ErrorAction SilentlyContinue
-        # $all_files | select  Name,VersionInfo | ConvertTo-Json -depth 100 -Compress | Out-File $allrecurse
+# utility
 
-        $allverinfo_json_path = "${ver}-versioninfo.json"
-        $allacl_json_path = "${ver}-acl.json"
-        $allrpc_json_path = "${ver}-rpc.json"
+
+## from https://hinchley.net/articles/an-approach-for-managing-microsoft-applocker-policies
+
+$FSR = [System.Security.AccessControl.FileSystemRights]
+
+# Unfortunately the FileSystemRights enum doesn't contain all the values from the Win32 API. Urgh.
+$GenericRights = @{
+  GENERIC_READ    = [int]0x80000000;
+  GENERIC_WRITE   = [int]0x40000000;
+  GENERIC_EXECUTE = [int]0x20000000;
+  GENERIC_ALL     = [int]0x10000000;
+  FILTER_GENERIC  = [int]0x0FFFFFFF;
+}
+
+# ... so we need to map them ourselves.
+$MappedGenericRights = @{
+  FILE_GENERIC_READ    = $FSR::ReadAttributes -bor $FSR::ReadData -bor $FSR::ReadExtendedAttributes -bor $FSR::ReadPermissions -bor $FSR::Synchronize
+  FILE_GENERIC_WRITE   = $FSR::AppendData -bor $FSR::WriteAttributes -bor $FSR::WriteData -bor $FSR::WriteExtendedAttributes -bor $FSR::ReadPermissions -bor $FSR::Synchronize
+  FILE_GENERIC_EXECUTE = $FSR::ExecuteFile -bor $FSR::ReadPermissions -bor $FSR::ReadAttributes -bor $FSR::Synchronize
+  FILE_GENERIC_ALL     = $FSR::FullControl
+}
+
+Function Map-GenericRightsToFileSystemRights([System.Security.AccessControl.FileSystemRights]$Rights) {
+  $MappedRights = New-Object -TypeName $FSR
+
+  if ($Rights -band $GenericRights.GENERIC_EXECUTE) {
+    $MappedRights = $MappedRights -bor $MappedGenericRights.FILE_GENERIC_EXECUTE
+  }
+
+  if ($Rights -band $GenericRights.GENERIC_READ) {
+   $MappedRights = $MappedRights -bor $MappedGenericRights.FILE_GENERIC_READ
+  }
+
+  if ($Rights -band $GenericRights.GENERIC_WRITE) {
+    $MappedRights = $MappedRights -bor $MappedGenericRights.FILE_GENERIC_WRITE
+  }
+
+  if ($Rights -band $GenericRights.GENERIC_ALL) {
+    $MappedRights = $MappedRights -bor $MappedGenericRights.FILE_GENERIC_ALL
+  }
+
+  return (($Rights -band $GenericRights.FILTER_GENERIC) -bor $MappedRights) -as $FSR
+}
+
+# These are the rights from the FileSystemRights enum we care about.
+$WriteRights = @('WriteData', 'CreateFiles', 'CreateDirectories', 'WriteExtendedAttributes', 'WriteAttributes', 'Write', 'Modify', 'FullControl')
+
+
+function isWriteableByIdentStr([System.Security.AccessControl.AuthorizationRuleCollection]$access,[string]$identStr){
+
+        $is_writeable = $false
+        $access |     
+        ? {$_.identityreference -imatch $identStr} | %{                
+          (map-genericrightstofilesystemrights $_.filesystemrights).tostring().split(",") | %{
+                if ($writerights -contains $_.trim()) {
+                        $is_writeable = $true
+                }
+          }
+        }
+        return $is_writeable
+}
+### end utility
+
+$runner=$args[0]
+$limit=$args[1]
+$ver = [System.Environment]::OSVersion.Version -join '.'
+$bin_types = "*.exe","*.dll","*.sys","*.winmd","*.cpl","*.ax","*.node","*.ocx","*.efi","*.acm","*.scr","*.tsp","*.drv"
+$allpaths_to_scan = "C:\Program*\Mi*","C:\Program*\Win*", "C:\Windows\System32\", "C:\Program*\Common*"
+$all_files = Get-ChildItem $allpaths_to_scan -Include $bin_types -Recurse -ErrorAction SilentlyContinue
+
+
+$jsonBase = @{}
+$allverinfo_json_path = "${runner}-${ver}-versioninfo.json"
+$has_getrpc = (Get-Command 'Get-RpcServer' -errorAction SilentlyContinue)
+
+Write-Host has get-rpc $has_getrpc
+Write-Host processing $all_files.Count
+# these files cause a block when processed with get-rpc
+$skip_rpc_files = "dpnaddr.dll"
+
+$count = 0
+foreach ($file in $all_files) {
+
+        # break after limit
+        if ($limit)
+        {
+          if ($count -gt $limit) {
+            break
+          }
+        }
+
+        $count++
+        Write-Host $file
+        Write-Host complete '%' ($count / $all_files.Count)        
+        $file_data = @{}
+        $sha256 = ($file | Get-FileHash).Hash
+        Write-Host $sha256
+        $verinfo = $file | select  Name,VersionInfo
+        $acl = $file | Get-Acl 
+        $acl_access = $acl.Access
+        $acl = $acl | select Owner,Group,Sddl,AccessToString,Audit
+        $is_parent_user_writeable = isWriteableByIdentStr (($file.Directory | Get-Acl).Access) ".*USERS|EVERYONE"
+        if ($has_getrpc) 
+        {
+            if (-Not $skip_rpc_files.contains($file.Name)) 
+            {
+                $rpc_server = $file | Get-RpcServer
+                $rpc_info = $rpc_server | select InterfaceId, InterfaceVersion, TransferSyntaxId, TransferSyntaxVersion, ProcedureCount, Server, Name, ServiceName, ServiceDisplayName, IsServiceRunning, Endpoints, EndpointCount, Client 
+                $rpc_procs = $rpc_server | Select-Object -ExpandProperty Procedures | select Name
+                $rpc_info | Add-Member -MemberType NoteProperty -Name "Procedures" -Value $procs.Name
+            }
+        }
+
         
+        $file_data.Add("verinfo", $verinfo)
+        $file_data.Add("acl", $acl)
+        $file_data.Add("rpc", $rpc_info)
+        $file_data.Add("isparentuserwriteable", $is_parent_user_writeable)
+        $file_data.Add("parentfolder",$file.Directory.fullname)
+        $jsonBase.Add($sha256, $file_data)
+      }
 
-        $all_verinfo = $all_files | select  Name,VersionInfo
-        $all_verinfo | ConvertTo-Json -depth 100 -Compress | Out-File $allverinfo_json_path
-        
-        $all_acl = $all_files | Get-Acl | select Owner,Group,Access,Sddl,AccessToString,Audit
+Write-Host writing json...
+Write-Host $jsonBase.Count objects
+$jsonBase | ConvertTo-Json -Depth 3 | Out-File $allverinfo_json_path
 
-        # write files
-        #$all_verinfo | ConvertTo-Json -depth 100 -Compress | Out-File $allverinfo_json_path
-        $all_acl | ConvertTo-Json -depth 100 -Compress | Out-File $allacl_json_path
-
-
-        # get rpc info
-        #Install-Module -Name NtObjectManager
-        Set-GlobalSymbolResolver -DbgHelpPath 'C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\dbghelp.dll' 
-        $all_rpc = $all_files | Get-RpcServer
-        $all_rpc | select InterfaceId, InterfaceVersion, TransferSyntaxId, TransferSyntaxVersion, ProcedureCount, Procedures, Server, FilePath, Name, ServiceName, ServiceDisplayName, IsServiceRunning, Endpoints, EndpointCount, Client | ConvertTo-Json -depth 3  | Out-File $allrpc_json_path
+# tar.gz json
+tar czf ($allverinfo_json_path + '.tar.gz') $allverinfo_json_path
