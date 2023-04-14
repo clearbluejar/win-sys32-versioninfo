b0dd357509ff3caed083fc108e6953e923e00839
clearbluejar <3752074+clearbluejar@users.noreply.github.com>
2023-04-14 17:00:08 -0400

    anotherworking

--- get_verinfo.ps1
+++ get_verinfo.ps1
@@ -30,7 +30,7 @@ Function Map-GenericRightsToFileSystemRights([System.Security.AccessControl.File
   }
 
   if ($Rights -band $GenericRights.GENERIC_READ) {
-   $MappedRights = $MappedRights -bor $MappedGenericRights.FILE_GENERIC_READ
+    $MappedRights = $MappedRights -bor $MappedGenericRights.FILE_GENERIC_READ
   }
 
   if ($Rights -band $GenericRights.GENERIC_WRITE) {
@@ -48,102 +48,122 @@ Function Map-GenericRightsToFileSystemRights([System.Security.AccessControl.File
 $WriteRights = @('WriteData', 'CreateFiles', 'CreateDirectories', 'WriteExtendedAttributes', 'WriteAttributes', 'Write', 'Modify', 'FullControl')
 
 
-function isWriteableByIdentStr([System.Security.AccessControl.AuthorizationRuleCollection]$access,[string]$identStr){
+function isWriteableByIdentStr([System.Security.AccessControl.AuthorizationRuleCollection]$access, [string]$identStr) {
 
-        $is_writeable = $false
-        $access |     
-        ? {$_.identityreference -imatch $identStr} | %{                
-          (map-genericrightstofilesystemrights $_.filesystemrights).tostring().split(",") | %{
-                if ($writerights -contains $_.trim()) {
-                        $is_writeable = $true
-                }
-          }
-        }
-        return $is_writeable
+  $is_writeable = $false
+  $access |     
+  ? { $_.identityreference -imatch $identStr } | % {                
+          (map-genericrightstofilesystemrights $_.filesystemrights).tostring().split(",") | % {
+      if ($writerights -contains $_.trim()) {
+        $is_writeable = $true
+      }
+    }
+  }
+  return $is_writeable
 }
 ### end utility
 
-$runner=$args[0]
-$paths=$args[1]
-$limit=$args[2]
+$runner = $args[0]
+$paths = $args[1]
+$limit = $args[2]
 
 $ver = [System.Environment]::OSVersion.Version -join '.'
-$bin_types = "*.exe","*.dll","*.sys","*.winmd","*.cpl","*.ax","*.node","*.ocx","*.efi","*.acm","*.scr","*.tsp","*.drv"
+$bin_types = "*.exe", "*.dll", "*.sys", "*.winmd", "*.cpl", "*.ax", "*.node", "*.ocx", "*.efi", "*.acm", "*.scr", "*.tsp", "*.drv"
 
-if ($paths)
-{
+if ($paths) {
   $allpaths_to_scan = $paths
 }
 else {
-  $allpaths_to_scan = "C:\Windows\System32\","C:\Program*\Mi*","C:\Program*\Win*","C:\Program*\Common*"
+  $allpaths_to_scan = "C:\Windows\System32\", "C:\Program Files (x86)\Mi*", "C:\Program Files (x86)\Win*", "C:\Program Files\Mi*", , "C:\Program Files\Win*", "C:\Program*\Common*"
 }
 
 
 Write-Host Starting: Collecting files...
 Write-Host folders: $allpaths_to_scan
 
-$all_files = Get-ChildItem $allpaths_to_scan -Include $bin_types -Recurse -ErrorAction SilentlyContinue | select  Name,VersionInfo,DirectoryName,PSPath,FullName
 
 $jsonBase = @{}
 $allverinfo_json_path = "${runner}-${ver}-versioninfo.json"
 $allverinfo_json_enhanced_path = "${runner}-${ver}-versioninfo.enhanced.json"
 $has_getrpc = (Get-Command 'Get-RpcServer' -errorAction SilentlyContinue)
+$cpu_count = (Get-CimInstance Win32_ComputerSystem).NumberOfLogicalProcessors
 
 Write-Host has get-rpc $has_getrpc
 Write-Host processing $all_files.Count
 Write-Host limit: $limit
+Write-Host cpus: $cpu_count
 # these files cause a block when processed with get-rpc
 $skip_rpc_files = "dpnaddr.dll"
-
 $count = 0
+
+# clear jobs
+Remove-Job -Force *
+
+foreach ($path in $allpaths_to_scan ) {
+    
+
+  $running = @(Get-Job | Where-Object { $_.State -eq 'Running' })
+  Write-Host Jobs runing count: $running.Count
+  
+  # don't limit cpus, as we are doing so much disk IO, let it run...
+  # if ($running.Count -ge $cpu_count) {
+  #     Write-Host waiting to start next job...
+  #     $running | Wait-Job -Any | Out-Null
+  # }
+
+  Write-Host "Starting job for $path"
+  Start-Job {
+    # do something with $using:server. Just sleeping for this example.
+    $files = Get-ChildItem $allpaths_to_scan -Include $bin_types -Recurse -ErrorAction SilentlyContinue | select  Name, VersionInfo, DirectoryName, PSPath, FullName
+    
+    return $files
+  } | Out-Null
+}
+
+$running = @(Get-Job | Where-Object { $_.State -eq 'Running' })
+Write-Host Collecting Waiting for all $running.Count jobs...
+# Wait for all jobs to complete and results ready to be received
+Wait-Job * | Out-Null
+
+
 
 foreach ($file in $all_files) {
 
-        # break after limit
-        if ($limit)
-        {
-          if ($count -gt $limit) {
-            break
-          }
-        }
 
-        $count++
-        Write-Host $file.FullName
-        Write-Host complete '%' ($count / $all_files.Count)       
-        $key = ($file | Get-FileHash).Hash
+  $count++
+  Write-Host $file.FullName
+  Write-Host complete '%' ($count / $all_files.Count)       
+  $key = ($file | Get-FileHash).Hash
 
-        if ($jsonBase.ContainsKey($key))
-        {
-          Write-Host Skipping $key .. already added
-          continue
-        }
-        Write-Host $key
+  if ($jsonBase.ContainsKey($key)) {
+    Write-Host Skipping $key .. already added
+    continue
+  }
+  Write-Host $key
         
-        $acl = $file | Get-Acl 
-        $acl_access = $acl.Access
-        $acl = $acl | select Owner,Group,Sddl,AccessToString,Audit
-        $is_parent_user_writeable = isWriteableByIdentStr (($file.DirectoryName | Get-Acl).Access) ".*USERS|EVERYONE"
+  $acl = $file | Get-Acl 
+  $acl_access = $acl.Access
+  $acl = $acl | select Owner, Group, Sddl, AccessToString, Audit
+  $is_parent_user_writeable = isWriteableByIdentStr (($file.DirectoryName | Get-Acl).Access) ".*USERS|EVERYONE"
         
-        if ($has_getrpc) 
-        {
-            if (-Not $skip_rpc_files.contains($file.Name)) 
-            {
-                $rpc_server = $file | Get-RpcServer
-                $rpc_info = $rpc_server | select InterfaceId, InterfaceVersion, TransferSyntaxId, TransferSyntaxVersion, ProcedureCount, Server, Name, ServiceName, ServiceDisplayName, IsServiceRunning, Endpoints, EndpointCount, Client 
-                $rpc_procs = $rpc_server | Select-Object -ExpandProperty Procedures | select Name
-                $rpc_info | Add-Member -MemberType NoteProperty -Name "Procedures" -Value $procs.Name
-            }
-        }
+  if ($has_getrpc) {
+    if (-Not $skip_rpc_files.contains($file.Name)) {
+      $rpc_server = $file | Get-RpcServer
+      $rpc_info = $rpc_server | select InterfaceId, InterfaceVersion, TransferSyntaxId, TransferSyntaxVersion, ProcedureCount, Server, Name, ServiceName, ServiceDisplayName, IsServiceRunning, Endpoints, EndpointCount, Client 
+      $rpc_procs = $rpc_server | Select-Object -ExpandProperty Procedures | select Name
+      $rpc_info | Add-Member -MemberType NoteProperty -Name "Procedures" -Value $procs.Name
+    }
+  }
 
-        $file_data = @{}        
-        $file_data.Add("VersionInfo", $file.VersionInfo)
-        $file_data.Add("acl", $acl)
-        $file_data.Add("rpc", $rpc_info)
-        $file_data.Add("isparentuserwriteable", $is_parent_user_writeable)
-        $file_data.Add("parentfolder",$file.Directory.fullname)
-        $file_data.Add("Name",$file.Name)
+  $file_data = @{}        
+  $file_data.Add("VersionInfo", $file.VersionInfo)
+  $file_data.Add("acl", $acl)
+  $file_data.Add("rpc", $rpc_info)
+  $file_data.Add("isparentuserwriteable", $is_parent_user_writeable)
+  $file_data.Add("parentfolder", $file.Directory.fullname)
+  $file_data.Add("Name", $file.Name)
 
-        $jsonBase.Add($key, $file_data)
+  $jsonBase.Add($key, $file_data)
         
 }
 
--- para.ps1
+++ para.ps1
@@ -1,36 +1,127 @@
-$allpaths_to_scan = "C:\Windows\System32\","C:\Program*\Common*"
+$init_funcs = {
+    function isWriteableByIdentStr([System.Security.AccessControl.AuthorizationRuleCollection]$access, [string]$identStr) {
+
+        return 0
+    }
+
+    function test1() {
+        return 0
+    }
+
+}
 
 
-Get-Job | Receive-job -AutoRemoveJob -Wait
-foreach ($server in $allpaths_to_scan ) {
+$allpaths_to_scan = "C:\Windows\System32\spool", "C:\Program Files (x86)\Mi*", "C:\Program Files (x86)\Win*" #, "C:\Program Files\Mi*", "C:\Program Files\Win*", "C:\Program*\Common*"
+$bin_types = "*.exe", "*.dll", "*.sys", "*.winmd", "*.cpl", "*.ax", "*.node", "*.ocx", "*.efi", "*.acm", "*.scr", "*.tsp", "*.drv"
+$cpu_count = (Get-CimInstance Win32_ComputerSystem).NumberOfLogicalProcessors
+$has_getrpc = (Get-Command 'Get-RpcServer' -errorAction SilentlyContinue)
+# clear jobs
+Remove-Job -Force *
+
+
+foreach ($path in $allpaths_to_scan ) {
+    
+
     # $running = @(Get-Job | Where-Object { $_.State -eq 'Running' })
-    # if ($running.Count -ge 2) {
+    # Write-Host Jobs runing count: $running.Count
+    # if ($running.Count -ge $cpu_count) {
+    #     Write-Host waiting to start next job...
     #     $running | Wait-Job -Any | Out-Null
     # }
 
-    Write-Host "Starting job for $server"
-    Start-Job {
+    Write-Host "Starting job for $path"
+    Start-Job  -InitializationScript $init_funcs {
         # do something with $using:server. Just sleeping for this example.
-        Start-Sleep 5
+        $all_files = gci -Recurse -Depth 0 -include $using:bin_types $using:path -ErrorAction SilentlyContinue | select  Name, VersionInfo, DirectoryName, PSPath, FullName
 
-        $files = gci $using:server -Depth 0
+        $count = 0        
+        $jsonBase = @{}        
 
-        return $files
+        $paths_processed = @()
+        foreach ($file in $all_files) {
+
+            $count++
+            #Write-Host $file.FullName
+            $complete = (($count / $all_files.Count) * 100)
+            #     Write-Host complete '%' $complete
+            
+          
+            if ($paths_processed.Contains($file.FullName)) {
+                Write-Host Skipping $file.FullName .. already added
+                continue
+            }
+            else {
+                $key = ($file | Get-FileHash).Hash
+
+                if ($jsonBase.ContainsKey($key)) {
+                    Write-Host Skipping $file.FullName .. already added
+                    continue
+                }
+
+                Write-Host $key
+                $acl = $file | Get-Acl                 
+                $acl = $acl | Select-Object Owner, Group, Sddl, AccessToString, Audit
+                $is_parent_user_writeable = isWriteableByIdentStr (($file.DirectoryName | Get-Acl).Access) ".*USERS|EVERYONE"
+                    
+                if ($has_getrpc) {
+                    if (-Not $skip_rpc_files.contains($file.Name)) {
+                        $rpc_server = $file | Get-RpcServer
+                        $rpc_info = $rpc_server | select InterfaceId, InterfaceVersion, TransferSyntaxId, TransferSyntaxVersion, ProcedureCount, Server, Name, ServiceName, ServiceDisplayName, IsServiceRunning, Endpoints, EndpointCount, Client 
+                        $rpc_procs = $rpc_server | Select-Object -ExpandProperty Procedures | select Name
+                        $rpc_info | Add-Member -MemberType NoteProperty -Name "Procedures" -Value $procs.Name
+                    }
+                }
+            
+                $file_data = @{}        
+                $file_data.Add("VersionInfo", $file.VersionInfo)
+                $file_data.Add("acl", $acl)
+                $file_data.Add("rpc", $rpc_info)
+                $file_data.Add("isparentuserwriteable", $is_parent_user_writeable)
+                $file_data.Add("parentfolder", $file.Directory.fullname)
+                $file_data.Add("Name", $file.Name)     
+                
+                
+                $jsonBase.Add($key, $file_data)
+
+            }
+
+            if ($count -gt 105) {
+                break
+            }
+            
+            Write-Progress -Activity $using:path -Status hi -PercentComplete $complete
+            
+        }
+
+
+        return  $jsonBase
+
     } | Out-Null
 }
 
+$running = @(Get-Job | Where-Object { $_.State -eq 'Running' })
+Write-Host Waiting for all $running.Count jobs...
 # Wait for all jobs to complete and results ready to be received
 Wait-Job * | Out-Null
 
+$jsonBase = @{}
+$mergedJson = @{}
 # Process the results
-foreach($job in Get-Job)
-{
-    $result = Receive-Job $job
-    Write-Host $result
+foreach ($job in Get-Job) {
+    $json = Receive-Job $job
+
+    Write-Host jobid: $job.id
+    Write-Host files count: $json.Count
+
+    $mergedJson += $json   
+
 }
 
 Remove-Job -State Completed
 
+write-host $mergedJson.Count
+
+$mergedJson | ConvertTo-Json | Out-File para.json
 
 #1..1000 | ForEach-Object -Parallel { "Hello: $_" } 
 
